4295f04f992a1f714b1b9f99c7407d8ef7e431f2
ahamathbasha <bashachristopherallah786@gmail.com>
2025-08-12 11:46:35 +0530

    logical questions practiced

--- JavascriptLogic/practice.js
+++ JavascriptLogic/practice.js
@@ -33,31 +33,68 @@
 
 //3. flat the object 
 
-const obj = {
-  newObj: {
-    obj2: {
-      obj5: {
-        one: 1,
-      },
-    },
-  },
-  obj3: {
-    obj4: {
-      two: 2,
-    },
-  },
-};
+// const obj = {
+//   newObj: {
+//     obj2: {
+//       obj5: {
+//         one: 1,
+//       },
+//     },
+//   },
+//   obj3: {
+//     obj4: {
+//       two: 2,
+//     },
+//   },
+// };
 
-function flatObject(obj,parentKey='',result={}){
-    for(let key in obj){
-        const fullKey = parentKey ? `${parentKey}.${key}` : key
-        if(typeof obj[key] == 'object' && obj[key] != null && !Array.isArray(obj[key])){
-            flatObject(obj[key],fullKey,result)
-        }else{
-            result[fullKey] = obj[key]
-        }
-    }
-    return result
+
+// function flatObj(obj,parentKey='',result={}){
+//   for(let key in obj){
+//     const fullPath = parentKey ? `${parentKey}.${key}` :key
+//     if(typeof obj[key] == 'object' && obj[key] != null && !Array.isArray(obj[key])){
+//       flatObj(obj[key],fullPath,result)
+//     }else{
+//       result[fullPath] = obj[key]
+//     }
+//   }
+//   return result
+// }
+
+// console.log(flatObj(obj))
+
+// function flatObject(obj,parentKey='',result={}){
+//     for(let key in obj){
+//         const fullKey = parentKey ? `${parentKey}.${key}` : key
+//         if(typeof obj[key] == 'object' && obj[key] != null && !Array.isArray(obj[key])){
+//             flatObject(obj[key],fullKey,result)
+//         }else{
+//             result[fullKey] = obj[key]
+//         }
+//     }
+//     return result
+// }
+
+// console.log(flatObject(obj))
+
+//4.Find the smallest sum and delete
+
+let arr = [[1,2,3],[-10,9,8],[-1,1,0]]
+let sum = Infinity
+let index = 0
+for(let i=0;i<arr.length;i++){
+
+  let compare = 0
+  for(let j=i+1;j<arr[i].length;j++){
+    compare += arr[i][j]
+  }
+
+  sum = Math.min(sum,compare)
+  if(compare == sum){
+    index = i
+  }
 }
 
-console.log(flatObject(obj))
+arr.splice(index,1)
+
+console.log(arr)
