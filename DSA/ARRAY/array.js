47c9468f0b5bf63e7fab2f2ae56297033916d64d
ahamathbasha <bashachristopherallah786@gmail.com>
2025-10-09 23:44:48 +0530

    bubble sort and min heap revised

--- DSA/ARRAY/array.js
+++ DSA/ARRAY/array.js
@@ -334,3 +334,20 @@ let arr=[[1,2,-3],[-11,2,3], [8,-10,2]]
 // arr.splice(index,1)
 // console.log(arr)
 
+
+function bubbleSort(arr){
+    let swapped = true
+    while(swapped){
+        swapped = false
+        for(let i=0;i<arr.length-1;i++){
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+                swapped = true
+            }
+        }
+    }
+
+    return arr
+}
--- DSA/HEAP/minheap.js
+++ DSA/HEAP/minheap.js
@@ -1,17 +1,173 @@
-class heap{
+// class heap{
+//     constructor(){
+//         this.heap=[]
+//     }
+
+//     getParentIndex(index){
+//         return Math.floor((index-1)/2)
+//     }
+
+//     getLeftIndex(index){
+//         return 2 * index + 1
+//     }
+
+//     getRightIndex(index){
+//         return 2 * index + 2
+//     }
+
+//     getParent(index){
+//         return this.heap[this.getParentIndex(index)]
+//     }
+
+//     getLeftChild(index){
+//         return this.heap[this.getLeftIndex(index)]
+//     }
+
+//     getRightChild(index){
+//         return this.heap[this.getRightIndex(index)]
+//     }
+
+//     hasParent(index){
+//         return this.getParentIndex(index) >= 0
+//     }
+
+//     hasLeftChild(index){
+//         return this.getLeftIndex(index) < this.heap.length
+//     }
+
+//     hasRightChild(index){
+//         return this.getRightIndex(index) < this.heap.length
+//     }
+
+//     swap(index1,index2){
+//         let temp=this.heap[index1]
+//         this.heap[index1]=this.heap[index2]
+//         this.heap[index2]=temp
+//     }
+
+//     peek(){
+//         if(this.heap.length == 0){
+//             return 'heap empty'
+//         }
+
+//         return this.heap[0]
+//     }
+
+//     print(){
+//         console.log(this.heap)
+//     }
+
+//     add(value){
+//         this.heap.push(value)
+//         this.heapifyUp()
+//     }
+
+//     heapifyUp(){
+//         let index=this.heap.length-1
+
+//         while(this.hasParent(index) && this.getParent(index) > this.heap[index]){
+//             this.swap(this.getParentIndex(index),index)
+//             index=this.getParentIndex(index)
+//         }
+//     }
+
+//     remove(){
+//         if(this.heap.length == 0){
+//             return 'heap empty'
+//         }
+
+//         let val=this.heap[0]
+//         this.heap[0]=this.heap[this.heap.length-1]
+//         this.heap.pop()
+//         this.heapifyDown()
+//         return val
+//     }
+
+//     heapifyDown(){
+//         let index=0
+
+//         while(this.hasLeftChild(index)){
+//             let smallerIndex=this.getLeftIndex(index)
+
+//             if(this.hasRightChild(index) && this.getRightChild(index) < this.getLeftChild(index)){
+//                 smallerIndex=this.getRightIndex(index)
+//             }
+
+//             if(this.heap[index] < this.heap[smallerIndex]){
+//                 break
+//             }
+//             else{
+//                 this.swap(index,smallerIndex)
+//             }
+
+//             index=smallerIndex
+//         }
+//     }
+
+//     heapSort(arr){
+//         const h=new heap()
+
+//         for(let i=0;i<arr.length;i++){
+//             h.add(arr[i])
+//         }
+
+//         let sortedArr=[]
+
+//         while(h.heap.length){
+//             sortedArr.push(h.remove())
+//         }
+
+//         return sortedArr
+//     }
+
+//     findKthLargest(arr,k){
+//         const h=new heap()
+//         for(let i=0;i<arr.length;i++){
+//             h.add(arr[i])
+//             if(h.heap.length > k){
+//                 h.remove()
+//             }
+//         }
+
+//         return h.peek()
+//     }
+// }
+
+// const h=new heap()
+
+// h.add(10);
+// h.add(5);
+// h.add(3);
+// h.add(1);
+// h.print(); // [1, 5, 3, 10]
+
+// h.remove();
+// h.print(); // [3, 5, 10]
+
+// console.log(h.heapSort([1, 9, 10, 4, 5])); // [1, 4, 5, 9, 10]
+
+// console.log(h.findKthLargest([1,9,10,4,5],3));
+
+
+
+class MinHeap{
     constructor(){
-        this.heap=[]
+        this.heap = []
+    }
+
+    isEmpty(){
+        return this.heap.length == 0
     }
 
     getParentIndex(index){
         return Math.floor((index-1)/2)
     }
 
-    getLeftIndex(index){
+    getLeftChildIndex(index){
         return 2 * index + 1
     }
 
-    getRightIndex(index){
+    getRightChildIndex(index){
         return 2 * index + 2
     }
 
@@ -20,11 +176,11 @@ class heap{
     }
 
     getLeftChild(index){
-        return this.heap[this.getLeftIndex(index)]
+        return this.heap[this.getLeftChildIndex(index)]
     }
 
     getRightChild(index){
-        return this.heap[this.getRightIndex(index)]
+        return this.heap[this.getRightChildIndex(index)]
     }
 
     hasParent(index){
@@ -32,29 +188,25 @@ class heap{
     }
 
     hasLeftChild(index){
-        return this.getLeftIndex(index) < this.heap.length
+        return this.getLeftChildIndex(index) < this.heap.length
     }
 
     hasRightChild(index){
-        return this.getRightIndex(index) < this.heap.length
+        return this.getRightChildIndex(index) < this.heap.length
     }
 
     swap(index1,index2){
-        let temp=this.heap[index1]
-        this.heap[index1]=this.heap[index2]
-        this.heap[index2]=temp
+        let temp = this.heap[index1]
+        this.heap[index1] = this.heap[index2]
+        this.heap[index2] = temp
     }
 
     peek(){
-        if(this.heap.length == 0){
-            return 'heap empty'
+        if(this.isEmpty()){
+            return null
         }
 
         return this.heap[0]
-    }
-
-    print(){
-        console.log(this.heap)
     }
 
     add(value){
@@ -63,87 +215,44 @@ class heap{
     }
 
     heapifyUp(){
-        let index=this.heap.length-1
+        let index = this.heap.length - 1
 
         while(this.hasParent(index) && this.getParent(index) > this.heap[index]){
-            this.swap(this.getParentIndex(index),index)
-            index=this.getParentIndex(index)
+            this.swap(index,this.getParentIndex(index))
+            index = this.getParentIndex(index)
         }
     }
 
+
     remove(){
-        if(this.heap.length == 0){
-            return 'heap empty'
+        const min = this.heap[0]
+        const end = this.heap.pop()
+
+        if(!this.isEmpty()){
+            this.heap[0] = end
+            this.heapifyDown()
         }
 
-        let val=this.heap[0]
-        this.heap[0]=this.heap[this.heap.length-1]
-        this.heap.pop()
-        this.heapifyDown()
-        return val
+        return min
     }
 
     heapifyDown(){
-        let index=0
-
+        let index = 0
+        
         while(this.hasLeftChild(index)){
-            let smallerIndex=this.getLeftIndex(index)
-
-            if(this.hasRightChild(index) && this.getRightChild(index) < this.getLeftChild(index)){
-                smallerIndex=this.getRightIndex(index)
+            let smallest = this.getLeftChildIndex(index)
+            if(this.hasRightChild && this.getRightChild(index) < this.getLeftChild(index)){
+                smallest = this.getRightChildIndex(index)
             }
 
-            if(this.heap[index] < this.heap[smallerIndex]){
+            if(this.heap[index] <= this.heap[smallest]){
                 break
             }
             else{
-                this.swap(index,smallerIndex)
+                this.swap(index,smallest)
             }
 
-            index=smallerIndex
+            index = smallest
         }
-    }
-
-    heapSort(arr){
-        const h=new heap()
-
-        for(let i=0;i<arr.length;i++){
-            h.add(arr[i])
-        }
-
-        let sortedArr=[]
-
-        while(h.heap.length){
-            sortedArr.push(h.remove())
-        }
-
-        return sortedArr
-    }
-
-    findKthLargest(arr,k){
-        const h=new heap()
-        for(let i=0;i<arr.length;i++){
-            h.add(arr[i])
-            if(h.heap.length > k){
-                h.remove()
-            }
-        }
-
-        return h.peek()
     }
 }
-
-const h=new heap()
-
-h.add(10);
-h.add(5);
-h.add(3);
-h.add(1);
-h.print(); // [1, 5, 3, 10]
-
-h.remove();
-h.print(); // [3, 5, 10]
-
-console.log(h.heapSort([1, 9, 10, 4, 5])); // [1, 4, 5, 9, 10]
-
-console.log(h.findKthLargest([1,9,10,4,5],3));
