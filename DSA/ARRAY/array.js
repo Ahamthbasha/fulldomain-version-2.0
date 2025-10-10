519d5b756a22ec381d88dce9b74133d51b7048f2
ahamathbasha <bashachristopherallah786@gmail.com>
2025-10-10 23:50:17 +0530

    quickSort revised

--- DSA/ARRAY/array.js
+++ DSA/ARRAY/array.js
@@ -351,3 +351,22 @@ function bubbleSort(arr){
 
     return arr
 }
+
+function quickSort(arr){
+    if(arr.length < 2){
+        return arr
+    }
+    let pivot = arr[arr.length-1]
+    let leftArr = []
+    let rightArr = []
+
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i] < pivot){
+            leftArr.push(arr[i])
+        }else{
+            rightArr.push(arr[i])
+        }
+    }
+
+    return [...quickSort(leftArr),pivot,...quickSort(rightArr)]
+}
